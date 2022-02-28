ea71bf52b335df27c1ee1ff829b5eaa74dc21c40
Yusuf İşleyen <me@yusufisleyen.com>
2022-02-28 23:23:16 +0300

    Swagger settings passive

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -5,16 +5,16 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Swagger settings
-app.UseSwagger();
-app.UseSwaggerUI(c =>
-{
-    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TotoApi V1");
-});
+//app.UseSwagger();
+//app.UseSwaggerUI(c =>
+//{
+//    c.SwaggerEndpoint("/swagger/v1/swagger.json", "TotoApi V1");
+//});
 
 // HttGet
 app.MapGet("/", () => "Hello Minimal APIs");
